d0048c163bba9e73083dd3f187c8a2571d4f4793
KurshidBanu-Basheer <lisci.learning@gmail.com>
2026-03-25 18:28:33 +0530

    Added ResearchPage

---
+++ src/components/ResearchPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ResearchPage = () => {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold mb-6">
+        Research & Internship Projects
+      </h1>
+
+      <p>Yaha tumhare projects aayenge...</p>
+    </div>
+  );
+};
+
+export default ResearchPage;
